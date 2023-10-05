ca1330ad2705d2c7f74752d29e883f7486c55d48
tejasnoovosoft <tejas.noovosoft@gmail.com>
2023-10-05 10:13:06 +0530

    Add Spring Boot Day1 Practice Project

---
+++ day4/src/main/kotlin/com/example/day4/Day4Application.kt
@@ -0,0 +1,11 @@
+package com.example.day4
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Day4Application
+
+fun main(args: Array<String>) {
+    runApplication<Day4Application>(*args)
+}
